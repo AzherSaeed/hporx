5704d392862a0b8fe7dfc9db94efbe96ffd0e766
TabashDeveloper <rana.tabash@gmail.com>
2022-03-03 14:34:39 +0500

    Design show case

--- src/App.js
+++ src/App.js
@@ -8,37 +8,41 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import MainPage from './components/AgenciesPage/MainPage';
 
 
 function App() {
   const [country , setCountry] = useState('')
   const [state , setState ] = useState('')
 
-  useEffect(() => {
-    // fetch(' https://ip.nf/me.json')
-    fetch('http://ip-api.com/json')
-    .then( res => res.json())
-    .then(response => {
-     console.log("Country is : ", response.country);
-     setCountry(response.country)
-     console.log("Region is: ", response.regionName);
-      setState(response.regionName)
-   })
-   .catch((data, status) => {
-     console.log('Request failed:', data);
-   });
- },[])
+//   useEffect(() => {
+//     // fetch(' https://ip.nf/me.json')
+//     fetch('http://ip-api.com/json')
+//     .then( res => res.json())
+//     .then(response => {
+//      console.log("Country is : ", response.country);
+//      setCountry(response.country)
+//      console.log("Region is: ", response.regionName);
+//       setState(response.regionName)
+//    })
+//    .catch((data, status) => {
+//      console.log('Request failed:', data);
+//    });
+//  },[])
 
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/">
+        <Route>
+        <MainPage/>
+          </Route>
+          {/* <Route exact path="/">
           <Homepage country={country} region={state}/>
           </Route>
           <Route  path="/home">
           <Homepage2 />
-          </Route>  
+          </Route>   */}
         </Switch>
       </div>
     </Router>
---
+++ src/components/AgenciesPage/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DesignServices from './DesignServices/DesignServices';
+
+
+function MainPage(){
+    return(<DesignServices/>);
+}
+export default MainPage;
